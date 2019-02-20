7ab9dd13474603f2b78cbd6c513e22fbd17d047e
Sam Feng <samantha.feng@cognite.com>
2019-02-20 15:29:57 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+     name='samfeng279-test',  
+     version='0.1',
+     scripts=['test'] ,
+     author="Sam Feng",
+     author_email="samfeng279@gmail.com",
+     description="Testing for deployment of Python packages",
+     url="https://github.com/s38feng/dea-extractor-public",
+     packages=['package'],
+ )
