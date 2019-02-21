965b0a670d5a9baef0c87cea27ed734216b64120
Sam Feng <samantha.feng@cognite.com>
2019-02-21 10:09:51 +0100

    Add dependencies to sample Python package

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ import setuptools
 setuptools.setup(
      name='samfeng279-test',  
      version='0.1',
-     scripts=['test'] ,
      author="Sam Feng",
      author_email="samfeng279@gmail.com",
      description="Testing for deployment of Python packages",
      url="https://github.com/s38feng/dea-extractor-public",
      packages=['package'],
+     install_requires=[
+          pandas,
+          cognite-sdk
+     ]
  )
