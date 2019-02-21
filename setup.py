ddc9207d966db9f462ab15cdba1b391ef9e1c518
Sam Feng <samantha.feng@cognite.com>
2019-02-21 10:13:52 +0100

    Added quotes around dependency names

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import setuptools
 
 setuptools.setup(
-     name='samfeng279-test',  
-     version='0.1',
-     author="Sam Feng",
-     author_email="samfeng279@gmail.com",
-     description="Testing for deployment of Python packages",
-     url="https://github.com/s38feng/dea-extractor-public",
-     packages=['package'],
-     install_requires=[
-          pandas,
-          cognite-sdk
-     ]
+    name='samfeng279-test',
+    version='0.1',
+    author="Sam Feng",
+    author_email="samfeng279@gmail.com",
+    description="Testing for deployment of Python packages",
+    url="https://github.com/s38feng/dea-extractor-public",
+    packages=['package'],
+    install_requires=[
+        'pandas',
+        'cognite-sdk'
+    ]
  )
